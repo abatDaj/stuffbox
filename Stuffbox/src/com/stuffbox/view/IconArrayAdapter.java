274eee8c96d4699664ae9608f71b8090f912755f
Willi <wifali@arcor.de>
2014-06-19 21:59:31 +0200

    icon-auswahl. Funktioiert noch nicht.

---
+++ Stuffbox/src/com/stuffbox/view/IconArrayAdapter.java
@@ -0,0 +1,37 @@
+package com.stuffbox.view;
+
+import com.stuffbox.R;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class IconArrayAdapter extends ArrayAdapter <String> {
+	private final Context context;
+	  private final String[] values;
+
+	  public IconArrayAdapter(Context context, String[] values) {
+	    super(context,R.layout.new_category_spinner_row , values);
+	    this.context = context;
+	    this.values = values;
+	  }
+
+	  @Override
+	  public View getView(int position, View convertView, ViewGroup parent) {
+	    LayoutInflater inflater = (LayoutInflater) context
+	        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	    View rowView = inflater.inflate(R.layout.new_category_spinner_row, parent, false);
+	    TextView textView = (TextView) rowView.findViewById(R.id.spinner_text_new_category);
+	    ImageView imageView = (ImageView) rowView.findViewById(R.id.spinner_icon_new_category);
+	    textView.setText(values[position]);
+	    // Change the icon for Windows and iPhone
+	    String s = values[position];
+	    imageView.setImageResource(R.drawable.icon_angry_minion);
+
+	    return rowView;
+	  }
+}
--- Stuffbox/src/com/stuffbox/view/NewCategoryActivity.java
+++ Stuffbox/src/com/stuffbox/view/NewCategoryActivity.java
@@ -1,9 +1,14 @@
 package com.stuffbox.view;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import com.stuffbox.R;
 import com.stuffbox.R.id;
 import com.stuffbox.R.layout;
 import com.stuffbox.R.menu;
+import com.stuffbox.controller.Controller;
+import com.stuffbox.model.FeatureType;
 
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
@@ -16,6 +21,8 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.os.Build;
 
@@ -27,6 +34,28 @@ public class NewCategoryActivity extends ActionBarActivity {
 		setContentView(R.layout.category_new);
 		//Intent intent = getIntent();
 		//((TextView)(findViewById(R.id.categoryText))).setText("Es wurde "+intent.getStringExtra(MainActivity.EXTRA_KATEGORIE_NAME)+ " gewählt!");
+		
+		ArrayList<FeatureType> types = Controller.getTypes();
+		
+		Spinner spinner = (Spinner) findViewById(R.id.spinner_new_category_icon);
+		// Create an ArrayAdapter using the string array and a default spinner layout
+		
+		/*ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+		        R.array.planets_array, android.R.layout.simple_spinner_item);
+		// Specify the layout to use when the list of choices appears
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		// Apply the adapter to the spinner
+		spinner.setAdapter(adapter);
+		*/
+
+	    String[] planets = new String[] { "Mercury", "Venus", "Earth", "Mars",
+                "Jupiter", "Saturn", "Uranus", "Neptune"};  
+		ArrayList<String> planetList = new ArrayList<String>();
+		planetList.addAll( Arrays.asList(planets) );
+		IconArrayAdapter adapter = new IconArrayAdapter(this,  planets);		
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spinner.setAdapter(adapter);
+
 	}
 
 	@Override
