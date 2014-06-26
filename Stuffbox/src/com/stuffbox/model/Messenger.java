a502e5dfdfb7b09f1a524cde02e01550588999d6
Aaron Joellenbeck <Vidar@134.102.148.200>
2014-06-26 11:16:08 +0200

    Messenger erweitert

--- Stuffbox/src/com/stuffbox/model/Messenger.java
+++ Stuffbox/src/com/stuffbox/model/Messenger.java
@@ -1,10 +1,26 @@
 package com.stuffbox.model;
 
+import android.app.AlertDialog;
+import android.content.Context;
+
 /**
  * 
  * @author Aaron Joellenbeck
  *Klasse die Hinweise und Nachrichten fuer den Benutzer darstellt.
  */
 public class Messenger {
-
+		AlertDialog alertMessage;
+	public Messenger(Context context){
+		AlertDialog.Builder alertmessage = new AlertDialog.Builder(context);
+	}
+	
+	private void setAlertMessage(String title, String message){
+		alertMessage.setTitle(title);
+		alertMessage.setMessage(message); 
+	}
+	
+	private void showAlertMessage(String title, String message){
+		this.setAlertMessage(title, message);
+		alertMessage.isShowing();
+	}
 }
