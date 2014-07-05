fa704a594ecd62086d5ef96433ee4f6a241f8056
Aaron Joellenbeck <Vidar@134.102.113.50>
2014-07-05 15:32:57 +0200

    Levelsystem begonnen

---
+++ Stuffbox/src/com/stuffbox/model/Level.java
@@ -0,0 +1,47 @@
+package com.stuffbox.model;
+
+/**
+ * 
+ * @author Dana Jenett, Elene Maier, Wilhelm, Aaron Joellenbeck
+ * Klasse die die Levelgrenzen fuer das Hauptlevel und die Abzeichen enthaelt 
+ *
+ */
+public class Level {
+	
+	private static final int badgeMark1 = 1; //Limit damit Badge 1 verliehen wird
+	private static final int badgeMark2 = 2; //Limit damit Badge 2 verliehen wird
+	private static final int badgeMark3 = 3; //Limit damit Badge 3 verliehen wird
+	private static final int badgeMark4 = 4; //Limit damit Badge 4 verliehen wird
+	private static final int badgeMark5 = 6; //Limit damit Badge 5 verliehen wird
+	private static final int LevelMark = 2; //Badges bis zum Levelaufstieg
+	
+	public Level(){
+		
+	}
+
+	public static int getBadgemark1() {
+		return badgeMark1;
+	}
+
+	public static int getBadgemark2() {
+		return badgeMark2;
+	}
+
+	public static int getBadgemark3() {
+		return badgeMark3;
+	}
+
+	public static int getBadgemark4() {
+		return badgeMark4;
+	}
+
+	public static int getBadgemark5() {
+		return badgeMark5;
+	}
+
+	public static int getLevelmark() {
+		return LevelMark;
+	}
+
+	
+}
